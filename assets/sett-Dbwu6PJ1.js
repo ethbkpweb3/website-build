7c0d2df86e938df58259d652b2f0d2b3d7d142e8
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-10-17 03:50:17 +0530

    ww

--- assets/sett-Dbwu6PJ1.js
+++
@@ -1,3 +0,0 @@
-const s = "/images/icons/menus/api.svg"
-  , a = "/images/svg/sett.svg";
-export {a as _, s as a};
--- presale/assets/google-play-BbEUCb8h.js
+++
@@ -1,1 +0,0 @@
-const s="/images/svg/apple.svg",g="/images/svg/google-play.svg";export{s as _,g as a};
