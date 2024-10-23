45fe1424121a5d5705b7e847c920d2be8289a059
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-10-23 20:36:15 +0530

    s

--- assets/sleep-MiAo3PcT.js
+++ assets/sleep-MiAo3PcT.js
@@ -1,8 +1,1 @@
-function e(e = 1e3) {
-    return new Promise((t => {
-        setTimeout(t, e)
-    }))
-}
-export {
-    e as s
-};
+function e(e=1e3){return new Promise((t=>{setTimeout(t,e)}))}export{e as s};
